8692f7e27d183119c3f24b6b2829034f710a4ab2
Ryota85 <65002143+Ryota85@users.noreply.github.com>
2020-07-25 01:10:53 +0900

    Create calculate.py

---
+++ calculate.py
@@ -0,0 +1,42 @@
+operation = input('''
+Please type in math operation you would like to complete:
++ for Addition
+- for substraction
+* for multiplication
+/ for division
+''')
+number_1 = int(input('Enter your first number:'))
+number_2 = int(input('Enter your second number:'))
+#Addition(足し算)
+if operation== '+':
+    print('{}+{}='.format(number_1,number_2))
+    print(number_1+number_2)
+elif operation== '-':
+    print('{}-{}='.format(number_1,number_2))
+    print(number_1-number_2)
+elif operation== '*':
+    print('{}*{}='.format(number_1,number_2))
+    print(number_1*number_2)
+elif operation== '/':
+    print('{}/{}='.format(number_1,number_2))
+    print(number_1/number_2)
+else:
+    print('You have not typed a valid operator, please run the program again')
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for No.
+''')
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+def welcome():
+    print('''
+Welcome to calculator
+''')
+welcome()
